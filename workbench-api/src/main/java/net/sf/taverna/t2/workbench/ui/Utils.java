363c850a32757e5bdf73eef5982d28a9296e2d63
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-02 07:59:37 +0000

    getParentFrame only needs Component, not Container
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-api/trunk@7196 bf327186-88b3-11dd-a302-d386e5130c1c

--- workbench-api/src/main/java/net/sf/taverna/t2/workbench/ui/Utils.java
+++ workbench-api/src/main/java/net/sf/taverna/t2/workbench/ui/Utils.java
@@ -1,10 +1,10 @@
 package net.sf.taverna.t2.workbench.ui;
 
-import java.awt.Container;
+import java.awt.Component;
 import java.awt.Frame;
 
 public class Utils {
-	public static Frame getParentFrame(Container container) {
+	public static Frame getParentFrame(Component container) {
 		while (container != null && !(container instanceof Frame)) {
 			container = container.getParent();
 		}
