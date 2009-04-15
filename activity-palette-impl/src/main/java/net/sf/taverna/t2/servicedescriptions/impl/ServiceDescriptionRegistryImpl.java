1d886e81202da33f8b7c79ee5e5a2548bafdd28e
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-15 14:20:53 +0000

    support configurable providers
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-impl/trunk@7268 bf327186-88b3-11dd-a302-d386e5130c1c

--- activity-palette-impl/src/main/java/net/sf/taverna/t2/servicedescriptions/impl/ServiceDescriptionRegistryImpl.java
+++ activity-palette-impl/src/main/java/net/sf/taverna/t2/servicedescriptions/impl/ServiceDescriptionRegistryImpl.java
@@ -1,24 +1,31 @@
 package net.sf.taverna.t2.servicedescriptions.impl;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import net.sf.taverna.t2.servicedescriptions.ConfigurableServiceDescriptionProvider;
 import net.sf.taverna.t2.servicedescriptions.ServiceDescription;
 import net.sf.taverna.t2.servicedescriptions.ServiceDescriptionProvider;
 import net.sf.taverna.t2.servicedescriptions.ServiceDescriptionRegistry;
 import net.sf.taverna.t2.spi.SPIRegistry;
+import net.sf.taverna.t2.workflowmodel.ConfigurationException;
+
+import org.apache.log4j.Logger;
 
 public class ServiceDescriptionRegistryImpl implements
 		ServiceDescriptionRegistry {
 
 	private static class Singleton {
-		private static final ServiceDescriptionRegistryImpl instance = 
-			new ServiceDescriptionRegistryImpl();
+		private static final ServiceDescriptionRegistryImpl instance = new ServiceDescriptionRegistryImpl();
 	}
 
 	public static ServiceDescriptionRegistryImpl getInstance() {
 		return Singleton.instance;
 	}
+
+	public static Logger logger = Logger
+			.getLogger(ServiceDescriptionRegistryImpl.class);
 
 	public SPIRegistry<ServiceDescriptionProvider> providerRegistry = new SPIRegistry<ServiceDescriptionProvider>(
 			ServiceDescriptionProvider.class);
@@ -39,9 +46,36 @@ public class ServiceDescriptionRegistryImpl implements
 		return serviceDescriptions;
 	}
 
+	public <ConfigBean> List<ConfigBean> getConfigurationsFor(
+			ConfigurableServiceDescriptionProvider<ConfigBean> provider) {
+		return provider.getDefaultConfigurations();
+	}
+
+	@SuppressWarnings("unchecked")
 	public Set<ServiceDescriptionProvider> getServiceDescriptionProviders() {
-		HashSet<ServiceDescriptionProvider> providers = new HashSet<ServiceDescriptionProvider>(
-				providerRegistry.getInstances());
+		HashSet<ServiceDescriptionProvider> providers = new HashSet<ServiceDescriptionProvider>();
+		for (ServiceDescriptionProvider provider : providerRegistry
+				.getInstances()) {
+			if (provider instanceof ConfigurableServiceDescriptionProvider) {
+				ConfigurableServiceDescriptionProvider template = ((ConfigurableServiceDescriptionProvider) provider);
+				List<Object> configurables = getConfigurationsFor(template);
+				for (Object config : configurables) {
+					// Make a copy that we can configure
+					ConfigurableServiceDescriptionProvider configurableProvider = template
+							.clone();
+					try {
+						configurableProvider.configure(config);
+					} catch (ConfigurationException e) {
+						logger.warn("Can't configure provider "
+								+ configurableProvider + " with " + config);
+						continue;
+					}
+					providers.add(configurableProvider);
+				}
+			} else {
+				providers.add(provider);
+			}
+		}
 		providers.addAll(localDescriptions);
 		return providers;
 	}
