cb6b3c7b23264d8ffcab9a2440c86044f588d50f
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-16 06:23:32 +0000

    property annotations for describing beans such as ServiceDescription
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-api/trunk@7275 bf327186-88b3-11dd-a302-d386e5130c1c

--- activity-palette-api/src/main/java/net/sf/taverna/t2/servicedescriptions/ServiceDescription.java
+++ activity-palette-api/src/main/java/net/sf/taverna/t2/servicedescriptions/ServiceDescription.java
@@ -4,30 +4,40 @@ import java.util.List;
 
 import javax.swing.Icon;
 
+import net.sf.taverna.t2.lang.beans.PropertyAnnotated;
+import net.sf.taverna.t2.lang.beans.PropertyAnnotation;
 import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
 
-public interface ServiceDescription<ConfigType> {
-	
+public abstract class ServiceDescription<ConfigType> extends PropertyAnnotated {
+
+	private static final String SERVICE_IMPLEMENTATION_CLASS = "Service implementation class";
+	private static final String SERVICE_CONFIGURATION = "Service configuration";
+	private static final String NAME = "Name";
 	public static final String SERVICE_TEMPLATES = "Service templates";
-	
-	public Class<? extends Activity<ConfigType>> getActivityClass();
 
-	public ConfigType getActivityConfiguration();
+	@PropertyAnnotation(expert = true, displayName = SERVICE_IMPLEMENTATION_CLASS)
+	public abstract Class<? extends Activity<ConfigType>> getActivityClass();
 
-	public String getName();
-	
-	public Icon getIcon();
-	
+	@PropertyAnnotation(expert = true, displayName = SERVICE_CONFIGURATION)
+	public abstract ConfigType getActivityConfiguration();
+
+	@PropertyAnnotation(displayName = NAME)
+	public abstract String getName();
+
+	@PropertyAnnotation(expert = true)
+	public abstract Icon getIcon();
+
+	@PropertyAnnotation(expert = true)
 	@SuppressWarnings("unchecked")
-	public List<? extends Comparable> getPath();
-	
-	
-	public boolean isTemplateService();
-	
+	public abstract List<? extends Comparable> getPath();
+
+	@PropertyAnnotation(hidden = true)
+	public abstract boolean isTemplateService();
+
 	@Override
-	public boolean equals(Object obj);
-	
+	public abstract boolean equals(Object obj);
+
 	@Override
-	public int hashCode();
-	
+	public abstract int hashCode();
+
 }
