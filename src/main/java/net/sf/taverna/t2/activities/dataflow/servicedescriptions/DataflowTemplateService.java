0a9f7b87680c8ce21dab8e401696261214f12e68
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-21 10:40:40 +0000

    Added Nested Workflow for service panel
    git-svn-id: http://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-activities/trunk/dataflow-activity-ui@7316 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ src/main/java/net/sf/taverna/t2/activities/dataflow/servicedescriptions/DataflowTemplateService.java
@@ -0,0 +1,35 @@
+package net.sf.taverna.t2.activities.dataflow.servicedescriptions;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import net.sf.taverna.t2.activities.dataflow.DataflowActivity;
+import net.sf.taverna.t2.activities.dataflow.query.DataflowActivityItem;
+import net.sf.taverna.t2.servicedescriptions.AbstractTemplateService;
+import net.sf.taverna.t2.workflowmodel.Dataflow;
+import net.sf.taverna.t2.workflowmodel.EditsRegistry;
+
+public class DataflowTemplateService extends AbstractTemplateService<Dataflow>{
+	
+	private static final String DATAFLOW = "Nested Workflow";
+
+	@Override
+	public Class<DataflowActivity> getActivityClass() {
+		return DataflowActivity.class;
+	}
+
+	@Override
+	public Dataflow getActivityConfiguration() {
+		return EditsRegistry.getEdits().createDataflow();
+	}
+
+	@Override
+	public Icon getIcon() {
+		return new ImageIcon(DataflowActivityItem.class.getResource("/dataflow.png"));
+	}
+
+	public String getName() {
+		return DATAFLOW;
+	}
+
+}
