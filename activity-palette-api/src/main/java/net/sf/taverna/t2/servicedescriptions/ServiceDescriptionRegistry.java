421afec98b0a4f5deec02a249fffb77becb94d28
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-22 16:38:34 +0000

    serialize added service providers, and load them at startup
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-api/trunk@7349 bf327186-88b3-11dd-a302-d386e5130c1c

--- activity-palette-api/src/main/java/net/sf/taverna/t2/servicedescriptions/ServiceDescriptionRegistry.java
+++ activity-palette-api/src/main/java/net/sf/taverna/t2/servicedescriptions/ServiceDescriptionRegistry.java
@@ -1,10 +1,12 @@
 package net.sf.taverna.t2.servicedescriptions;
 
+import java.io.File;
 import java.util.List;
 import java.util.Set;
 
 import net.sf.taverna.t2.lang.observer.Observable;
 import net.sf.taverna.t2.servicedescriptions.events.ServiceDescriptionRegistryEvent;
+import net.sf.taverna.t2.workflowmodel.serialization.DeserializationException;
 
 public interface ServiceDescriptionRegistry extends
 		Observable<ServiceDescriptionRegistryEvent> {
@@ -12,6 +14,8 @@ public interface ServiceDescriptionRegistry extends
 	public void addServiceDescriptionProvider(
 			ServiceDescriptionProvider provider);
 
+	public Set<ServiceDescriptionProvider> getLocalServiceProviders();
+	
 	public Set<ServiceDescriptionProvider> getServiceDescriptionProviders();
 
 	@SuppressWarnings("unchecked")
@@ -20,7 +24,19 @@ public interface ServiceDescriptionRegistry extends
 	@SuppressWarnings("unchecked")
 	public List<ConfigurableServiceProvider> getUnconfiguredServiceProviders();
 
+	public void loadServiceProviders() throws DeserializationException;
+
+	public void loadServiceProviders(File serviceProviderFile)
+			throws DeserializationException;
+
+	public void refresh();
+
 	public void removeServiceDescriptionProvider(
 			ServiceDescriptionProvider provider);
 
+	public void saveServiceDescriptions();
+
+	public void saveServiceDescriptions(File serviceDescriptionsFile);
+
+	
 }
