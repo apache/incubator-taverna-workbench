b198139a0c2c7867156d0b3ca60d51d8854abcd9
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-22 16:33:43 +0000

    show warning message (and remove provider from registry) if something goes wrong. show provider icon in dialogue owner Component is a Component, not a MenuSomething..
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-components/trunk@7341 bf327186-88b3-11dd-a302-d386e5130c1c

--- activity-palette-ui/src/main/java/net/sf/taverna/t2/workbench/ui/servicepanel/actions/AddServiceProviderAction.java
+++ activity-palette-ui/src/main/java/net/sf/taverna/t2/workbench/ui/servicepanel/actions/AddServiceProviderAction.java
@@ -5,7 +5,6 @@ package net.sf.taverna.t2.workbench.ui.servicepanel.actions;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
@@ -14,16 +13,23 @@ import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import net.sf.taverna.t2.lang.observer.Observable;
+import net.sf.taverna.t2.lang.observer.Observer;
 import net.sf.taverna.t2.lang.uibuilder.UIBuilder;
 import net.sf.taverna.t2.servicedescriptions.ConfigurableServiceProvider;
 import net.sf.taverna.t2.servicedescriptions.ServiceDescriptionRegistry;
-import net.sf.taverna.t2.workbench.ui.servicepanel.menu.AddServiceProviderMenu;
+import net.sf.taverna.t2.servicedescriptions.events.ProviderErrorNotification;
+import net.sf.taverna.t2.servicedescriptions.events.ServiceDescriptionProvidedEvent;
+import net.sf.taverna.t2.servicedescriptions.events.ServiceDescriptionRegistryEvent;
 import net.sf.taverna.t2.workflowmodel.ConfigurationException;
 
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.log4j.Logger;
 
 /**
  * Action for adding a service provider
@@ -34,47 +40,26 @@ import org.apache.commons.beanutils.PropertyUtils;
 @SuppressWarnings("serial")
 public class AddServiceProviderAction extends AbstractAction {
 
-	protected static Dimension DIALOG_SIZE = new Dimension(400, 300);
+	private static Logger logger = Logger
+			.getLogger(AddServiceProviderAction.class);
+
+	// protected static Dimension DIALOG_SIZE = new Dimension(400, 300);
 
 	private ServiceDescriptionRegistry serviceDescriptionRegistry;
-	
-	private Component owner;
 
-	private final class AddProviderAction extends AbstractAction {
-		private final Object configurationBean;
-		private final JDialog dialog;
 
-		private AddProviderAction(Object configurationBean, JDialog dialog) {
-			super("Add");
-			this.configurationBean = configurationBean;
-			this.dialog = dialog;
-		}
-
-		@SuppressWarnings("unchecked")
-		public void actionPerformed(ActionEvent e) {
-
-			ConfigurableServiceProvider cloned = confProvider.clone();
-			try {
-				cloned.configure(configurationBean);
-				getServiceDescriptionRegistry().addServiceDescriptionProvider(
-						cloned);
-				System.out.println("Added with " + configurationBean);
-				dialog.setVisible(false);
-			} catch (ConfigurationException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		}
-	}
 
 	@SuppressWarnings("unchecked")
 	private final ConfigurableServiceProvider confProvider;
 
+	private final Component owner;
+
 	@SuppressWarnings("unchecked")
-	public AddServiceProviderAction(ConfigurableServiceProvider confProvider, AddServiceProviderMenu addServiceProviderMenu) {
+	public AddServiceProviderAction(ConfigurableServiceProvider confProvider,
+			Component owner) {
 		super(confProvider.getName() + "...", confProvider.getIcon());
 		this.confProvider = confProvider;
-		this.owner = addServiceProviderMenu;
+		this.owner = owner;
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -88,21 +73,23 @@ public class AddServiceProviderAction extends AbstractAction {
 		JPanel buildEditor = buildEditor(configurationBean);
 		JDialog dialog = new JDialog();
 		dialog.setTitle("Add " + confProvider.getName());
-		dialog.add(buildEditor);
+		JPanel iconPanel = new JPanel();
+		iconPanel.add(new JLabel(confProvider.getIcon()), BorderLayout.NORTH);
+		dialog.add(iconPanel, BorderLayout.WEST);
+		dialog.add(buildEditor, BorderLayout.CENTER);
 		JPanel buttonPanel = new JPanel();
-		buttonPanel.add(
-				new JButton(new AddProviderAction(configurationBean, dialog)),
-				BorderLayout.WEST);
+		buttonPanel.add(new JButton(new AddProviderAction(configurationBean,
+				dialog)), BorderLayout.WEST);
 		dialog.add(buttonPanel, BorderLayout.SOUTH);
-//		dialog.setSize(buttonPanel.getPreferredSize());
+		// dialog.setSize(buttonPanel.getPreferredSize());
 		dialog.pack();
 		dialog.setLocationRelativeTo(owner);
-		dialog.setLocation(owner.getLocationOnScreen().x + owner.getWidth(), owner.getLocationOnScreen().y + owner.getHeight());
+		dialog.setLocation(owner.getLocationOnScreen().x + owner.getWidth(),
+				owner.getLocationOnScreen().y + owner.getHeight());
 		dialog.setVisible(true);
 	}
 
 	protected JPanel buildEditor(Object configurationBean) {
-
 		PropertyDescriptor[] properties;
 		try {
 			properties = PropertyUtils
@@ -110,14 +97,15 @@ public class AddServiceProviderAction extends AbstractAction {
 		} catch (Exception ex) {
 			throw new RuntimeException("Can't inspect configuration bean", ex);
 		}
-		List<String> uiBuilderConfig = new ArrayList<String>();		
+		List<String> uiBuilderConfig = new ArrayList<String>();
 		int lastPreferred = 0;
 		for (PropertyDescriptor property : properties) {
 			if (property.isHidden() || property.isExpert()) {
 				// TODO: Add support for expert properties
 				continue;
 			}
-			String propertySpec = property.getName() + ":name=" + property.getDisplayName();
+			String propertySpec = property.getName() + ":name="
+					+ property.getDisplayName();
 			if (property.isPreferred()) {
 				// Add it to the front
 				uiBuilderConfig.add(lastPreferred++, propertySpec);
@@ -138,4 +126,72 @@ public class AddServiceProviderAction extends AbstractAction {
 	public ServiceDescriptionRegistry getServiceDescriptionRegistry() {
 		return serviceDescriptionRegistry;
 	}
+	
+	public class AddProviderAction extends AbstractAction {
+
+		private final Object configurationBean;
+		private final JDialog dialog;
+
+		private AddProviderAction(Object configurationBean, JDialog dialog) {
+			super("Add");
+			this.configurationBean = configurationBean;
+			this.dialog = dialog;
+		}
+
+		@SuppressWarnings("unchecked")
+		public void actionPerformed(ActionEvent e) {
+
+			final ConfigurableServiceProvider cloned = confProvider.clone();
+			try {
+				cloned.configure(configurationBean);
+				getServiceDescriptionRegistry().addObserver(
+						new CheckAddedCorrectlyObserver(cloned));
+				getServiceDescriptionRegistry().addServiceDescriptionProvider(
+						cloned);
+				dialog.setVisible(false);
+			} catch (ConfigurationException e1) {
+				logger.warn("Can't configure provider " + cloned + " using "
+						+ configurationBean, e1);
+				JOptionPane
+						.showMessageDialog(null,
+								"Can't configure service provider "
+										+ cloned.getName(),
+								"Can't add service provider",
+								JOptionPane.ERROR_MESSAGE);
+
+			}
+		}
+	}
+
+	public class CheckAddedCorrectlyObserver implements
+			Observer<ServiceDescriptionRegistryEvent> {
+		@SuppressWarnings("unchecked")
+		private final ConfigurableServiceProvider provider;
+
+		@SuppressWarnings("unchecked")
+		private CheckAddedCorrectlyObserver(ConfigurableServiceProvider provider) {
+			this.provider = provider;
+		}
+
+		public void notify(Observable<ServiceDescriptionRegistryEvent> sender,
+				ServiceDescriptionRegistryEvent message) throws Exception {
+			if (message instanceof ProviderErrorNotification) {
+				ProviderErrorNotification errorMsg = (ProviderErrorNotification) message;
+				if (errorMsg.getProvider() == provider) {
+					getServiceDescriptionRegistry().removeObserver(this);
+					getServiceDescriptionRegistry()
+							.removeServiceDescriptionProvider(provider);
+					JOptionPane.showMessageDialog(null, errorMsg.getMessage(),
+							"Can't add provider " + provider,
+							JOptionPane.ERROR_MESSAGE);
+				}
+			} else if (message instanceof ServiceDescriptionProvidedEvent) {
+				ServiceDescriptionProvidedEvent providedMsg = (ServiceDescriptionProvidedEvent) message;
+				if (providedMsg.getProvider() == provider) {
+					getServiceDescriptionRegistry().removeObserver(this);
+				}
+			}
+		}
+	}
+
 }
