c2b35eeaebe7f1df21c99d4323eda08154c88ff5
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-27 16:33:50 +0000

    getTextualDescription() -> getDescription()
    equals() and hashcode() now implemented in abstract class as implementers seemed to just return false and 0.
    New abstract method getIdentifyingData() is to return a List of the data to use for equals and hashcode.
    
    isTemplateService() now defaults to false (AbstractTemplateService sets this to true)
    
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-api/trunk@7443 bf327186-88b3-11dd-a302-d386e5130c1c

--- activity-palette-api/src/main/java/net/sf/taverna/t2/servicedescriptions/ServiceDescription.java
+++ activity-palette-api/src/main/java/net/sf/taverna/t2/servicedescriptions/ServiceDescription.java
@@ -10,11 +10,30 @@ import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
 
 public abstract class ServiceDescription<ConfigType> extends PropertyAnnotated {
 
-	private static final String TEXTUAL_DESCRIPTION = "Textual description";
-	private static final String SERVICE_IMPLEMENTATION_CLASS = "Service implementation class";
-	private static final String SERVICE_CONFIGURATION = "Service configuration";
-	private static final String NAME = "Name";
 	public static final String SERVICE_TEMPLATES = "Service templates";
+	private static final String NAME = "Name";
+	private static final String SERVICE_CONFIGURATION = "Service configuration";
+	private static final String SERVICE_IMPLEMENTATION_CLASS = "Service implementation class";
+	private static final String DESCRIPTION = "Description";
+
+	private String description = "";
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object obj) {
+		if (! (obj instanceof ServiceDescription)) {
+			return false;
+		}
+		List<Object> myIdentifyingData = getIdentifyingData();
+		if (myIdentifyingData == null) {
+			return super.equals(obj);
+		}
+		if (! getClass().isInstance(obj) && obj.getClass().isInstance(this)) {
+			return false;
+		}
+		ServiceDescription serviceDescription = (ServiceDescription) obj;
+		return myIdentifyingData.equals(serviceDescription.getIdentifyingData());
+	}
 
 	@PropertyAnnotation(expert = true, displayName = SERVICE_IMPLEMENTATION_CLASS)
 	public abstract Class<? extends Activity<ConfigType>> getActivityClass();
@@ -22,36 +41,47 @@ public abstract class ServiceDescription<ConfigType> extends PropertyAnnotated {
 	@PropertyAnnotation(expert = true, displayName = SERVICE_CONFIGURATION)
 	public abstract ConfigType getActivityConfiguration();
 
-	@PropertyAnnotation(displayName = NAME)
-	public abstract String getName();
+	@PropertyAnnotation(displayName = DESCRIPTION)
+	public String getDescription() {
+		return this.description;
+	}
 
 	@PropertyAnnotation(expert = true)
 	public abstract Icon getIcon();
+
+	@PropertyAnnotation(displayName = NAME)
+	public abstract String getName();
+	
 
 	@PropertyAnnotation(expert = true)
 	@SuppressWarnings("unchecked")
 	public abstract List<? extends Comparable> getPath();
 
+	@Override
+	public int hashCode() {
+		List<Object> identifyingData = getIdentifyingData();
+		if (identifyingData == null) {
+			return super.hashCode();
+		}
+		return identifyingData.hashCode();
+	}
+
 	@PropertyAnnotation(hidden = true)
-	public abstract boolean isTemplateService();
-
-	@Override
-	public abstract boolean equals(Object obj);
-
-	@Override
-	public abstract int hashCode();
-
-	private String textualDescription = "";
+	public boolean isTemplateService() {
+		return false;
+	}
 	
 	/**
-	 * @param textualDescription the textualDescription to set
+	 * @param description the description to set
 	 */
-	public void setTextualDescription(String textualDescription) {
-		this.textualDescription = textualDescription;
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
-	@PropertyAnnotation(displayName = TEXTUAL_DESCRIPTION)
-	public String getTextualDescription() {
-		return this.textualDescription;
+	public String toString() {
+		return "Service description " +  getName();
 	}
+	
+	protected abstract List<Object> getIdentifyingData();
+	
 }
