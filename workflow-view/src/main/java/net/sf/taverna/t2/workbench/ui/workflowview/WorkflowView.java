ddb5a256ea78e43d2f865e504fb42e413bdcb6e9
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-05-11 12:27:15 +0000

    Reverted
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-components/trunk@7643 bf327186-88b3-11dd-a302-d386e5130c1c

--- workflow-view/src/main/java/net/sf/taverna/t2/workbench/ui/workflowview/WorkflowView.java
+++ workflow-view/src/main/java/net/sf/taverna/t2/workbench/ui/workflowview/WorkflowView.java
@@ -60,58 +60,49 @@ public abstract class WorkflowView extends JPanel implements UIComponentSPI{
 		Object bean = sd.getActivityConfiguration();
 		Activity activity;
 			activity = (Activity) sd.getActivityClass().newInstance();
-		
-		return importActivity(currentDataflow, activity, bean , component, rename);
+			String name = sd.getName()
+			.replace(' ', '_');
+	name = Tools.uniqueProcessorName(name, currentDataflow);
+	try {
+		List<Edit<?>> editList = new ArrayList<Edit<?>>();
+		editList.add(edits.getConfigureActivityEdit(activity, bean));
+		p=edits.createProcessor(name);
+		editList.add(edits.getDefaultDispatchStackEdit(p));
+		editList.add(edits.getAddActivityEdit(p, activity));
+//		editList.add(edits.getMapProcessorPortsForActivityEdit(p));
+//		editList.add(edits.getRenameProcessorEdit(p, name));
+		editList.add(edits.getAddProcessorEdit(currentDataflow, p));
+			editManager
+					.doDataflowEdit(currentDataflow, new CompoundEdit(editList));
+	} catch (EditException e) {
+		logger.warn("Could not add processor : edit error", e);
+		p = null;
 	}
-
-	@SuppressWarnings("unchecked")
-	public final static Processor importActivity(Dataflow currentDataflow, Activity activity, Object bean, JComponent component, boolean rename) throws InstantiationException, IllegalAccessException {
-		Processor p = null;
-
-		String name = sd.getName()
-				.replace(' ', '_');
-		name = Tools.uniqueProcessorName(name, currentDataflow);
-		
-		try {
-			List<Edit<?>> editList = new ArrayList<Edit<?>>();
-			editList.add(edits.getConfigureActivityEdit(activity, bean));
-			p=edits.createProcessor(name);
-			editList.add(edits.getDefaultDispatchStackEdit(p));
-			editList.add(edits.getAddActivityEdit(p, activity));
-//			editList.add(edits.getMapProcessorPortsForActivityEdit(p));
-//			editList.add(edits.getRenameProcessorEdit(p, name));
-			editList.add(edits.getAddProcessorEdit(currentDataflow, p));
-				editManager
-						.doDataflowEdit(currentDataflow, new CompoundEdit(editList));
-		} catch (EditException e) {
-			logger.warn("Could not add processor : edit error", e);
-			p = null;
-		}
-		
-		if ((p != null) && rename) {
-			RenameProcessorAction rpa = new RenameProcessorAction(currentDataflow, p, component);
-			rpa.actionPerformed(new ActionEvent(component, 0, ""));
-		}
-		
-		if ((p != null) && sd.isTemplateService()) {
-			JPopupMenu dummyMenu = MenuManager.getInstance().createContextMenu(currentDataflow,
-					p, null);
-			for (Component c : dummyMenu.getComponents()) {
-				logger.info(c.getClass().getCanonicalName());
-				if (c instanceof JMenuItem) {
-					JMenuItem menuItem = (JMenuItem) c;
-					Action action = menuItem.getAction();
-					if (action != null) {
-						logger.info(action.getClass().getCanonicalName());
-						if (action instanceof ActivityConfigurationAction) {
-							logger.info("Got appropriate action " + action.getClass().getCanonicalName());
-						action.actionPerformed(new ActionEvent(component, 0, ""));
-						break;
-					}
-				}
+	
+	if ((p != null) && rename) {
+		RenameProcessorAction rpa = new RenameProcessorAction(currentDataflow, p, component);
+		rpa.actionPerformed(new ActionEvent(component, 0, ""));
+	}
+	
+	if ((p != null) && sd.isTemplateService()) {
+		JPopupMenu dummyMenu = MenuManager.getInstance().createContextMenu(currentDataflow,
+				p, null);
+		for (Component c : dummyMenu.getComponents()) {
+			logger.info(c.getClass().getCanonicalName());
+			if (c instanceof JMenuItem) {
+				JMenuItem menuItem = (JMenuItem) c;
+				Action action = menuItem.getAction();
+				if (action != null) {
+					logger.info(action.getClass().getCanonicalName());
+					if (action instanceof ActivityConfigurationAction) {
+						logger.info("Got appropriate action " + action.getClass().getCanonicalName());
+					action.actionPerformed(new ActionEvent(component, 0, ""));
+					break;
 				}
 			}
+			}
 		}
-		return p;
-	}	
+	}
+	return p;
+}
 }
