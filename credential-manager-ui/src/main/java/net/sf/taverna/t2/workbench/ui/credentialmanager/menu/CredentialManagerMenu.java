678ac67130ad674d92028110bf833913308b8402
alex@mygrid.org.uk <alex@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-05-22 12:10:14 +0000

    Moved the Credential Manager UI bits to credential-manager-ui.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-components/trunk@7725 bf327186-88b3-11dd-a302-d386e5130c1c

--- credential-manager-ui/src/main/java/net/sf/taverna/t2/workbench/ui/credentialmanager/menu/CredentialManagerMenu.java
+++ credential-manager-ui/src/main/java/net/sf/taverna/t2/workbench/ui/credentialmanager/menu/CredentialManagerMenu.java
@@ -30,7 +30,7 @@ import net.sf.taverna.t2.workbench.ui.credentialmanager.action.CredentialManager
 public class CredentialManagerMenu extends AbstractMenuAction{
 
 	public CredentialManagerMenu() {
-		super(URI.create("http://taverna.sf.net/2008/t2workbench/menu#advanced"),100);
+		super(URI.create("http://taverna.sf.net/2008/t2workbench/menu#advanced"),60);
 	}
 
 	@Override
