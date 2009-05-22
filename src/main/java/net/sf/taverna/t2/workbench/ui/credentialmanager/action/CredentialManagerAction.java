792ecfdcb1488c9617f8a1c9519c9fa505804598
alex@mygrid.org.uk <alex@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-05-22 12:10:14 +0000

    Moved the Credential Manager UI bits to credential-manager-ui.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-components/trunk/credential-manager-ui@7725 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/workbench/ui/credentialmanager/action/CredentialManagerAction.java
+++ src/main/java/net/sf/taverna/t2/workbench/ui/credentialmanager/action/CredentialManagerAction.java
@@ -25,12 +25,12 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 
-import net.sf.taverna.t2.security.credentialmanager.ui.CredentialManagerGUI;
+import net.sf.taverna.t2.workbench.ui.credentialmanager.CredentialManagerGUI;
 
 @SuppressWarnings("serial")
 public class CredentialManagerAction extends AbstractAction {
 
-	private static ImageIcon ICON = new ImageIcon(CredentialManagerAction.class.getResource("/cred_manager16x16.png"));
+	private static ImageIcon ICON = new ImageIcon(CredentialManagerAction.class.getResource("/images/cred_manager16x16.png"));
 	
 	public CredentialManagerAction() {
 		super("Credential Manager",ICON);
