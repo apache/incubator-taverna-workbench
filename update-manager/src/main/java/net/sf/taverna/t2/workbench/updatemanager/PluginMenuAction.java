5b093b6f5bce4aa7054bc074143463c03f40c58e
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-09-14 12:47:33 +0000

    Added F12 keyboard shortcut as per T2-782
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-impl/trunk@8182 bf327186-88b3-11dd-a302-d386e5130c1c

--- update-manager/src/main/java/net/sf/taverna/t2/workbench/updatemanager/PluginMenuAction.java
+++ update-manager/src/main/java/net/sf/taverna/t2/workbench/updatemanager/PluginMenuAction.java
@@ -1,11 +1,15 @@
 package net.sf.taverna.t2.workbench.updatemanager;
 
 import java.awt.Component;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.net.URI;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.KeyStroke;
 
 import net.sf.taverna.raven.plugins.PluginManager;
 import net.sf.taverna.raven.plugins.ui.PluginManagerFrame;
@@ -21,6 +25,8 @@ public class PluginMenuAction extends AbstractMenuAction {
 
 		public SoftwareUpdates() {
 			super(UPDATES_AND_PLUGINS, UpdatesAvailableIcon.updateRecommendedIcon);
+			putValue(Action.ACCELERATOR_KEY,
+					KeyStroke.getKeyStroke(KeyEvent.VK_F12, 0));
 		}
 
 		public void actionPerformed(ActionEvent e) {
