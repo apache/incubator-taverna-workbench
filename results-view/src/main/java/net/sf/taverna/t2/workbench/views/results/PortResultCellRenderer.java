7493b8e2a087a6d06475b7740c743a6fd2009cba
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-12-04 16:31:02 +0000

    Changed to improve list presentation
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-components/trunk@9413 bf327186-88b3-11dd-a302-d386e5130c1c

--- results-view/src/main/java/net/sf/taverna/t2/workbench/views/results/PortResultCellRenderer.java
+++ results-view/src/main/java/net/sf/taverna/t2/workbench/views/results/PortResultCellRenderer.java
@@ -7,12 +7,14 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Label;
 
+import javax.swing.JLabel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.MutableTreeNode;
 import javax.swing.tree.TreeNode;
 
 import net.sf.taverna.t2.reference.T2Reference;
+import net.sf.taverna.t2.workbench.views.results.ResultTreeNode.ResultTreeNodeState;
 
 /**
  * @author alanrw
@@ -23,8 +25,26 @@ public class PortResultCellRenderer extends DefaultTreeCellRenderer {
 			Object value, boolean selected, boolean expanded,
 			boolean leaf, int row, boolean hasFocus) {
 		Component result = super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
-		if (value instanceof TreeNode) {
-			if (containsError((TreeNode) value)) {
+		if (value instanceof ResultTreeNode) {
+			ResultTreeNode value2 = (ResultTreeNode) value;
+			String text = "";
+			ResultTreeNode parent = (ResultTreeNode) value2.getParent();
+			if (value2.getState().equals(ResultTreeNodeState.RESULT_LIST)) {
+				text = "List";
+				if (!parent.getState().equals(ResultTreeNodeState.RESULT_TOP)) {
+					text += "" + (parent.getIndex(value2) + 1);
+				}
+				text += " with " + value2.getLeafCount() + " values";
+				if (value2.getLeafCount() != value2.getChildCount()) {
+					text += " in " + value2.getChildCount() + " sublists";
+				}
+			} else if (value2.getState().equals(ResultTreeNodeState.RESULT_REFERENCE)) {
+				text = "Result " + (parent.getIndex(value2) + 1);
+			} else if (value2.getState().equals(ResultTreeNodeState.RESULT_WAITING)) {
+				text = "Waiting for data";
+			}
+			((JLabel) result).setText(text);
+			if (containsError(value2)) {
 				result.setForeground(Color.RED);
 			}
 		}
