fc24c8953c956df9ba72dde9abf7d5e0ed06533e
alex@mygrid.org.uk <alex@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-03-23 12:23:39 +0000

    Removed selecting of the first available item in the result list of a port due to two problems: firslty if an item was too large then a popup dualog would appera immeditelly asking the user if they wanted to render it or cancel rendering; secondly - sometimes the first available item is not the fist item in the list so then it would become invisible even though it is selected as the list scrolls up as other result items become available.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-components/branches/ui-components-1.1@9836 bf327186-88b3-11dd-a302-d386e5130c1c

--- results-view/src/main/java/net/sf/taverna/t2/workbench/views/results/PortResultsViewTab.java
+++ results-view/src/main/java/net/sf/taverna/t2/workbench/views/results/PortResultsViewTab.java
@@ -105,6 +105,7 @@ public class PortResultsViewTab extends JPanel{
 							
 				// If nothing is currently selected in the tree - select either the
 				// result or the first AVAILABLE item in the result list
+				/*
 				if (tree.getSelectionRows() == null || tree.getSelectionRows().length == 0){ 			
 					ResultTreeNode parent = (ResultTreeNode)e.getTreePath().getLastPathComponent(); // parent of the changed node(s)
 					int[] indices = e.getChildIndices(); //indexes of the changed node(s)
@@ -112,7 +113,7 @@ public class PortResultsViewTab extends JPanel{
 					if (firstChild.getState().equals(ResultTreeNode.ResultTreeNodeState.RESULT_REFERENCE)){ // if this is the result node rather than result list placeholder
 						tree.setSelectionPath(new TreePath(firstChild.getPath())); // select this node
 					}
-				}
+				}*/
 			}
 
 			public void treeNodesInserted(TreeModelEvent e) {
