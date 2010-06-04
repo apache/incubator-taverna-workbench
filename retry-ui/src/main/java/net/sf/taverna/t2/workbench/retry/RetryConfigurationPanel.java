5e2dbcd2dc67af7a8c825f8ceeb82e8585b8728d
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-06-04 11:25:15 +0000

    T2-1360 use sensible defaults rather than 0 for retries
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-exts/trunk@10722 bf327186-88b3-11dd-a302-d386e5130c1c

--- retry-ui/src/main/java/net/sf/taverna/t2/workbench/retry/RetryConfigurationPanel.java
+++ retry-ui/src/main/java/net/sf/taverna/t2/workbench/retry/RetryConfigurationPanel.java
@@ -10,37 +10,71 @@ import javax.swing.JTextField;
 import net.sf.taverna.t2.workflowmodel.processor.dispatch.layers.RetryConfig;
 
 public class RetryConfigurationPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6625235577980124442L;
 
+	private static final double MIN_BACKOFF = 1.0;
+	private static final int MIN_DELAY = 1;
+	private static final int MIN_RETRIES = 0;
+
+	private final RetryConfig defaultConfig = new RetryConfig();
+	
 	private final RetryConfig configuration;
 	private JTextField maxRetriesField = new JTextField();
 	private JTextField initialDelayField = new JTextField();
 	private JTextField maximumDelayField = new JTextField();
 	private JTextField backoffFactorField = new JTextField();
+	private JPanel panel;
 
 	public RetryConfigurationPanel(RetryConfig configuration) {
 		this.configuration = configuration;
-		this.setLayout(new GridLayout(4,2));
-		
+		panel = new JPanel(new GridLayout(4,2));
+		add(panel);
 		populate();
 	}
 
-	public void populate() {
-		this.removeAll();
-		this.add(new JLabel("Maximum number of retries"));
-		maxRetriesField.setText(Integer.toString(configuration.getMaxRetries()));
-		this.add(maxRetriesField);
+	public void populate() {		
+		readConfiguration();
+		panel.removeAll();
+		panel.add(new JLabel("Maximum number of retries"));
+		panel.add(maxRetriesField);
+		panel.add(new JLabel("Initial delay in ms"));		
+		panel.add(initialDelayField);
+		panel.add(new JLabel("Maximum delay in ms"));
+		panel.add(maximumDelayField);
+		panel.add(new JLabel("Delay increase factor"));
+		panel.add(backoffFactorField);
+	}
+
+	private void readConfiguration() {
+		int maxRetries = configuration.getMaxRetries();
+		if (maxRetries < MIN_RETRIES) {
+			maxRetries = defaultConfig.getMaxRetries();
+		}
 		
-		this.add(new JLabel("Initial delay in ms"));
-		initialDelayField.setText(Integer.toString(configuration.getInitialDelay()));
-		this.add(initialDelayField);
+		int initialDelay = configuration.getInitialDelay();
+		if (initialDelay < MIN_DELAY) {
+			initialDelay = defaultConfig.getInitialDelay();
+		}
 		
-		this.add(new JLabel("Maximum delay in ms"));
-		maximumDelayField.setText(Integer.toString(configuration.getMaxDelay()));
-		this.add(maximumDelayField);
-		
-		this.add(new JLabel("Delay increase factor"));
-		backoffFactorField.setText(Float.toString(configuration.getBackoffFactor()));
-		this.add(backoffFactorField);
+		int maxDelay = configuration.getMaxDelay();
+		if (maxDelay < MIN_DELAY) {
+			maxDelay = defaultConfig.getMaxDelay();
+		}
+		if (maxDelay < initialDelay) {
+			maxDelay = initialDelay;
+		}
+		float backoffFactor = configuration.getBackoffFactor();
+		if (backoffFactor <= 1.0) {
+			backoffFactor = defaultConfig.getBackoffFactor();
+		}
+
+		maxRetriesField.setText(Integer.toString(maxRetries));
+		initialDelayField.setText(Integer.toString(initialDelay));
+		maximumDelayField.setText(Integer.toString(maxDelay));
+		backoffFactorField.setText(Float.toString(backoffFactor));
 	}
 
 	public boolean validateConfig() {
@@ -49,39 +83,45 @@ public class RetryConfigurationPanel extends JPanel {
 		int initialDelay = -1;
 		int maxDelay = -1;
 		float backoffFactor = -1;
+		
 		try {
 			maxRetries = Integer.parseInt(maxRetriesField.getText());
-			if (maxRetries < 0) {
+			if (maxRetries < MIN_RETRIES) {
 			    errorText += "The number of retries must be non-negative.\n";
 			}
 		}
 		catch (NumberFormatException e) {
 			errorText += "The maximum number of retries must be an integer.\n";
 		}
+		
 		try {
 			initialDelay = Integer.parseInt(initialDelayField.getText());
-			if (initialDelay < 1) {
+			if (initialDelay < MIN_DELAY) {
 				errorText += "The initial delay must be a positive integer.\n";
 			}
 		}
 		catch (NumberFormatException e) {
 			errorText += "The initial delay must be an integer.\n";
 		}
+		
 		try {
 			maxDelay = Integer.parseInt(maximumDelayField.getText());
-			if (maxDelay < 1) {
+			if (maxDelay < MIN_DELAY) {
 				errorText += "The maximum delay must be a positive integer.\n";
 			}
 			else if (maxDelay < initialDelay) {
 				errorText += "The maximum delay must be greater than the initial delay.\n";
+				maxDelay = Math.max(MIN_DELAY, initialDelay);
+				maximumDelayField.setText(Integer.toString(maxDelay));
 			}
 		}
 		catch (NumberFormatException e) {
 			errorText += "The maximum delay must be an integer.\n";
 		}
+		
 		try {
 			backoffFactor = Float.parseFloat(backoffFactorField.getText());
-			if (backoffFactor < 1.0) {
+			if (backoffFactor < MIN_BACKOFF) {
 				errorText += "The backoff factor must be greater than one.\n";
 			}
 		}
