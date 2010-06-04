41a7247f6e85757ee1f3f49618bdb3dbd93b8707
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-06-04 11:17:34 +0000

    GridLayout imported
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-api/trunk@10721 bf327186-88b3-11dd-a302-d386e5130c1c

--- report-api/src/main/java/net/sf/taverna/t2/workbench/report/view/ReportViewComponent.java
+++ report-api/src/main/java/net/sf/taverna/t2/workbench/report/view/ReportViewComponent.java
@@ -7,6 +7,7 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
