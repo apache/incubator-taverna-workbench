788265b4a29489e20426b9810051d903fe6a52a7
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-06-17 12:40:51 +0000

    T2-1416 error indication by colour of iterations
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-components/trunk@10948 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ results-view/src/main/java/net/sf/taverna/t2/workbench/views/results/processor/IterationTreeNode.java
@@ -0,0 +1,88 @@
+package net.sf.taverna.t2.workbench.views.results.processor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
+
+import org.apache.log4j.Logger;
+
+public class IterationTreeNode extends DefaultMutableTreeNode {
+	private static final long serialVersionUID = -7522904828725470216L;
+
+	private List<Integer> iteration;
+	private boolean hasErrors = false;
+
+	private static Logger logger = Logger.getLogger(IterationTreeNode.class);
+	
+	public IterationTreeNode() {
+		this.setIteration(new ArrayList<Integer>());
+	}
+	
+	public IterationTreeNode(List<Integer> iteration) {
+		this.setIteration(iteration);		
+	}
+
+	public void setIteration(List<Integer> iteration) {
+		this.iteration = iteration;
+	}
+
+	public List<Integer> getIteration() {
+		return iteration;
+	}
+	
+	public List<Integer> getParentIteration() {
+		TreeNode parentNode = getParent();
+		if (parentNode instanceof IterationTreeNode) {
+			IterationTreeNode iterationTreeNode = (IterationTreeNode) parentNode;
+			return iterationTreeNode.getIteration();
+		}
+		return null;		
+	}
+	
+	public boolean hasErrors() {
+		if (hasErrors) {
+			return true;
+		}
+		for (int i=0; i<getChildCount(); i++) {
+			TreeNode child = getChildAt(i);
+			if (! (child instanceof IterationTreeNode)) {
+				logger.error("Unexpected child: " + child);
+				continue;
+			}
+			IterationTreeNode iterationTreeNode = (IterationTreeNode) child;
+			if (iterationTreeNode.hasErrors()) {
+				hasErrors = true;
+				return true;
+			}			
+		}
+		return false;
+	}
+	
+	
+	public String toString(){
+		boolean isNested = getChildCount() > 0;
+		StringBuilder sb = new StringBuilder();		
+		if (! getIteration().isEmpty() || isNested) {
+			// Iteration 3.1.3
+			if (isNested) {
+				sb.append("Nested iteration ");
+			} else {
+				sb.append("Iteration ");
+			}
+			for (Integer index : getIteration()) {				
+				sb.append(index+1);
+				sb.append(".");
+			}
+			if (! getIteration().isEmpty()) {
+				// Remove last .
+				sb.delete(sb.length()-1, sb.length());
+			}
+		} else {
+			sb.append("Invocation");
+		}		
+		
+		return sb.toString();
+	}
+}
