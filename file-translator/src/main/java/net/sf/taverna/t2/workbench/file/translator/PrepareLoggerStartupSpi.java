db015f60188c57172e7953ae7442dc29cd76f70b
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-06-29 11:37:19 +0000

    Updated to code-freeze 2010-06-29
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-exts/branches/ui-exts-1.2@11060 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ file-translator/src/main/java/net/sf/taverna/t2/workbench/file/translator/PrepareLoggerStartupSpi.java
@@ -0,0 +1,20 @@
+package net.sf.taverna.t2.workbench.file.translator;
+
+import net.sf.taverna.t2.workbench.StartupSPI;
+import net.sf.taverna.utils.MyGridConfiguration;
+
+public class PrepareLoggerStartupSpi implements StartupSPI {
+
+	public int positionHint() {
+		// Before SetConsoleLoggerStartup 
+		// -- otherwise Taverna 1 libraries will cause double logging
+		return 5;
+	}
+
+	public boolean startup() {
+		// Causes Log4jConfiguration to be initialised
+		MyGridConfiguration.getInstance();
+		return true;
+	}
+
+}
