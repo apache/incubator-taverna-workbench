5000f3052354ed51105ba17f72fe495d63f582ee
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-08-19 13:22:52 +0000

    Converted SPIs to OSGi services
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-api/trunk@13750 bf327186-88b3-11dd-a302-d386e5130c1c

--- renderers-api/src/main/java/net/sf/taverna/t2/renderers/RendererRegistry.java
+++ renderers-api/src/main/java/net/sf/taverna/t2/renderers/RendererRegistry.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2007 The University of Manchester   
+ * Copyright (C) 2011 The University of Manchester   
  * 
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
@@ -20,63 +20,33 @@
  ******************************************************************************/
 package net.sf.taverna.t2.renderers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import net.sf.taverna.t2.invocation.InvocationContext;
 import net.sf.taverna.t2.reference.ReferenceService;
 import net.sf.taverna.t2.reference.T2Reference;
-import net.sf.taverna.t2.spi.SPIRegistry;
 
-import org.apache.log4j.Logger;
-
-public class RendererRegistry extends SPIRegistry<Renderer> {
-
-	private static Logger logger = Logger.getLogger(RendererRegistry.class);
-
-	public RendererRegistry() {
-		super(Renderer.class);
-	}
+/**
+ * 
+ * 
+ * @author David Withers
+ */
+public interface RendererRegistry {
 
 	/**
 	 * Get all of the available renderers for a specific MIME type. If there is
 	 * a problem with one then catch the exception and log the problem but carry
 	 * on since there is probably more than one way to render the data
-	 * 
+	 *
 	 * @param context.getReferenceService()
 	 * @param entityIdentifier
 	 * @param mimeType
 	 * @return
 	 */
-	public List<Renderer> getRenderersForMimeType(InvocationContext context,
-			T2Reference reference, String mimeType) {
-		ArrayList<Renderer> list = new ArrayList<Renderer>();
-		for (Renderer renderer : getInstances()) {
-			try {
-				if (renderer.canHandle(context.getReferenceService(), reference, mimeType)) {
-					list.add(renderer);
-				}
-			} catch (RendererException e) {
-				logger.warn("Problem with renderer for " + renderer.getType(),
-						e);
-			}
-		}
-		return list;
-	}
-	
-	public List<Renderer> getRenderersForMimeType(ReferenceService refernceService, T2Reference reference, String mimeType) {
-		ArrayList<Renderer> list = new ArrayList<Renderer>();
-		for (Renderer renderer : getInstances()) {
-			try {
-				if (renderer.canHandle(refernceService, reference, mimeType)) {
-					list.add(renderer);
-				}
-			} catch (RendererException e) {
-				logger.warn("Problem with renderer for " + renderer.getType(),
-						e);
-			}
-		}
-		return list;
-	}
+	public List<Renderer> getRenderersForMimeType(InvocationContext context, T2Reference reference,
+			String mimeType);
+
+	public List<Renderer> getRenderersForMimeType(ReferenceService refernceService,
+			T2Reference reference, String mimeType);
 
 }
