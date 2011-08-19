5a9706787866f4fd7d8a08363454eabc8cc17163
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-08-19 13:09:28 +0000

    Converted SPIs to OSGi services
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-api/trunk@13748 bf327186-88b3-11dd-a302-d386e5130c1c

--- file-api/src/main/java/net/sf/taverna/t2/workbench/file/DataflowPersistenceHandler.java
+++ file-api/src/main/java/net/sf/taverna/t2/workbench/file/DataflowPersistenceHandler.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright (C) 2007 The University of Manchester   
- * 
+ * Copyright (C) 2007 The University of Manchester
+ *
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1 of
  *  the License, or (at your option) any later version.
- *    
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *    
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
@@ -24,7 +24,6 @@ import java.io.File;
 import java.net.URL;
 import java.util.Collection;
 
-import net.sf.taverna.t2.spi.SPIRegistry;
 import net.sf.taverna.t2.workbench.file.exceptions.OpenException;
 import net.sf.taverna.t2.workbench.file.exceptions.SaveException;
 import net.sf.taverna.t2.workflowmodel.Dataflow;
@@ -34,9 +33,9 @@ import net.sf.taverna.t2.workflowmodel.Dataflow;
  * the {@link FileManager}.
  * <p>
  * </p>
- * 
+ *
  * @author Stian Soiland-Reyes
- * 
+ *
  */
 public interface DataflowPersistenceHandler {
 
@@ -44,7 +43,7 @@ public interface DataflowPersistenceHandler {
 	 * A collection of supported file types for
 	 * {@link #openDataflow(FileType, Object)}, or an empty collection if
 	 * opening is not supported by this handler.
-	 * 
+	 *
 	 * @return A collection of supported {@link FileType}s for opening.
 	 */
 	public Collection<FileType> getOpenFileTypes();
@@ -57,7 +56,7 @@ public interface DataflowPersistenceHandler {
 	 * For example, a handler that supports sources opened from a {@link File}
 	 * and {@link URL} could return
 	 * <code>Arrays.asList(File.class, URL.class)</code>
-	 * 
+	 *
 	 * @return A collection of supported {@link Class}es of the open source
 	 *         types.
 	 */
@@ -71,7 +70,7 @@ public interface DataflowPersistenceHandler {
 	 * For example, a handler that supports saving to destinations that are
 	 * instances of a {@link File} could return
 	 * <code>Arrays.asList(File.class)</code>
-	 * 
+	 *
 	 * @return A collection of supported {{@link Class}es of the save
 	 *         destination types.
 	 */
@@ -81,7 +80,7 @@ public interface DataflowPersistenceHandler {
 	 * A collection of supported file types for
 	 * {@link #saveDataflow(Dataflow, FileType, Object)}, or an empty collection
 	 * if saving is not supported by this handler.
-	 * 
+	 *
 	 * @return A collection of supported {@link FileType}s for saving.
 	 */
 	public Collection<FileType> getSaveFileTypes();
@@ -93,7 +92,7 @@ public interface DataflowPersistenceHandler {
 	 * The {@link FileType} will be equal to one of the types from
 	 * {@link #getOpenFileTypes()}, and the source class will be one that is
 	 * assignable to one of the classes from {@link #getOpenSourceTypes()}.
-	 * 
+	 *
 	 * @param fileType
 	 *            {@link FileType} determining which serialisation method has
 	 *            been used
@@ -115,7 +114,7 @@ public interface DataflowPersistenceHandler {
 	 * {@link #getSaveFileTypes()}, and the destination class will be one that
 	 * is assignable to one of the classes from
 	 * {@link #getSaveDestinationTypes()}.
-	 * 
+	 *
 	 * @param dataflow
 	 *            {@link Dataflow} to be saved
 	 * @param fileType
@@ -137,7 +136,7 @@ public interface DataflowPersistenceHandler {
 	 * destination, and the destination is different from last
 	 * {@link #openDataflow(FileType, Object)} or
 	 * {@link #saveDataflow(Dataflow, FileType, Object)} of the given dataflow.
-	 * 
+	 *
 	 * @param dataflow
 	 *            {@link Dataflow} that is to be saved
 	 * @param fileType
