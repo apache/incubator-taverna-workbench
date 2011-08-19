24a27ed95d3eec0aae63c39b8f2abb83f4823f71
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-08-19 11:45:28 +0000

    Converted ColourManager to OSGi service.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-api/trunk@13744 bf327186-88b3-11dd-a302-d386e5130c1c

--- configuration-api/src/main/java/net/sf/taverna/t2/workbench/configuration/colour/ColourManager.java
+++ configuration-api/src/main/java/net/sf/taverna/t2/workbench/configuration/colour/ColourManager.java
@@ -1,53 +1,44 @@
 /*******************************************************************************
- * Copyright (C) 2007 The University of Manchester   
- * 
+ * Copyright (C) 2011 The University of Manchester
+ *
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1 of
  *  the License, or (at your option) any later version.
- *    
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *    
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  ******************************************************************************/
-package net.sf.taverna.t2.workbench.configuration;
+package net.sf.taverna.t2.workbench.configuration.colour;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.Color;
 
-import net.sf.taverna.t2.spi.SPIRegistry;
+import net.sf.taverna.t2.workbench.configuration.Configurable;
 
-public class ConfigurationUIRegistry extends SPIRegistry<ConfigurationUIFactory>{
+/**
+ *
+ *
+ * @author David Withers
+ */
+public interface ColourManager extends Configurable {
 
-	private static ConfigurationUIRegistry instance = new ConfigurationUIRegistry();
+	/**
+	 * Builds a Color that has been configured and associated with the given String (usually an object type).
+	 *
+	 * @return the associated Color, or if nothing is associated returns WHITE
+	 *
+	 */
+	public Color getPreferredColour(String itemKey);
 
-	public static ConfigurationUIRegistry getInstance() {
-		return instance;
-	}
-	
-	private ConfigurationUIRegistry() {
-		super(ConfigurationUIFactory.class);
-	}
-	
-	public List<ConfigurationUIFactory> getConfigurationUIFactoriesForConfigurable(Configurable configurable) {
-		List<ConfigurationUIFactory> result = new ArrayList<ConfigurationUIFactory>();
-		for (ConfigurationUIFactory factory : getConfigurationUIFactories()) {
-			if (factory.canHandle(configurable.getUUID())) {
-				result.add(factory);
-			}
-		}
-		return result;
-	}
-	
-	public List<ConfigurationUIFactory> getConfigurationUIFactories() {
-		return getInstances();
-	}
+	public void setPreferredColour(String itemKey, Color colour);
+
 }
