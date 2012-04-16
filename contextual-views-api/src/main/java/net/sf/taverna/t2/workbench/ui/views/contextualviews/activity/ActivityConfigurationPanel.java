99417242f9b04f5697d809fe2f025d1c6cf52420
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-04-16 11:38:15 +0000

    Change for XStream
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-api/branches/maintenance@14798 bf327186-88b3-11dd-a302-d386e5130c1c

--- contextual-views-api/src/main/java/net/sf/taverna/t2/workbench/ui/views/contextualviews/activity/ActivityConfigurationPanel.java
+++ contextual-views-api/src/main/java/net/sf/taverna/t2/workbench/ui/views/contextualviews/activity/ActivityConfigurationPanel.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 /**
  * @author alanrw
@@ -23,13 +24,13 @@ public abstract class ActivityConfigurationPanel<A extends Activity, B extends O
 	public abstract void noteConfiguration();
 
 	protected String convertBeanToString(Object bean) {
-		XStream xstream = new XStream();
+ 	        XStream xstream = new XStream(new DomDriver());
 		xstream.setClassLoader(bean.getClass().getClassLoader());
 		return xstream.toXML(bean);
 	}
 	
 	protected Object cloneBean(Object bean) {
-		XStream xstream = new XStream();
+	        XStream xstream = new XStream(new DomDriver());
 		xstream.setClassLoader(bean.getClass().getClassLoader());
 		return (xstream.fromXML(xstream.toXML(bean)));
 	}
