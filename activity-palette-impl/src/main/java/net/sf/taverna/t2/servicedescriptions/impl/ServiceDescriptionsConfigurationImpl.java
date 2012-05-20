53fecd1308ba0c699c53ed31c67adffd8661c9a5
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-05-20 08:04:34 +0000

    Split api and implementation.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-impl/trunk@14953 bf327186-88b3-11dd-a302-d386e5130c1c

--- activity-palette-impl/src/main/java/net/sf/taverna/t2/servicedescriptions/impl/ServiceDescriptionsConfigurationImpl.java
+++ activity-palette-impl/src/main/java/net/sf/taverna/t2/servicedescriptions/impl/ServiceDescriptionsConfigurationImpl.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright (C) 2007 The University of Manchester   
- * 
+ * Copyright (C) 2007 The University of Manchester
+ *
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1 of
  *  the License, or (at your option) any later version.
- *    
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *    
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
@@ -23,19 +23,18 @@ package net.sf.taverna.t2.servicedescriptions.impl;
 import java.util.HashMap;
 import java.util.Map;
 
-import net.sf.taverna.t2.workbench.configuration.AbstractConfigurable;
+import net.sf.taverna.t2.servicedescriptions.ServiceDescriptionsConfiguration;
 
-public class ServiceDescriptionsConfig extends AbstractConfigurable {
+import uk.org.taverna.configuration.AbstractConfigurable;
+import uk.org.taverna.configuration.ConfigurationManager;
+
+public class ServiceDescriptionsConfigurationImpl extends AbstractConfigurable implements ServiceDescriptionsConfiguration  {
+
+	public ServiceDescriptionsConfigurationImpl(ConfigurationManager configurationManager) {
+		super(configurationManager);
+	}
 
 	private static final String INCLUDE_DEFAULTS = "includeDefaults";
-
-	private static class Singleton {
-		private static ServiceDescriptionsConfig instance = new ServiceDescriptionsConfig();
-	}
-
-	public static ServiceDescriptionsConfig getInstance() {
-		return Singleton.instance;
-	}
 
 	private static final String SERVICE_PALETTE = "Service providers";
 	private static final String SERVICE_PALETTE_PREFIX = "ServiceProviders";
@@ -47,6 +46,7 @@ public class ServiceDescriptionsConfig extends AbstractConfigurable {
 		return CATEGORY;
 	}
 
+	@Override
 	public Map<String, String> getDefaultPropertyMap() {
 		Map<String, String> defaults = new HashMap<String, String>();
 		defaults.put(INCLUDE_DEFAULTS, "true");
@@ -54,30 +54,36 @@ public class ServiceDescriptionsConfig extends AbstractConfigurable {
 		return defaults;
 	}
 
+	@Override
 	public String getDisplayName() {
 		return SERVICE_PALETTE;
 	}
-	
+
+	@Override
 	public String getFilePrefix() {
 		return SERVICE_PALETTE_PREFIX;
 	}
 
-	public String getUUID() {
+	@Override	public String getUUID() {
 		return UUID;
 	}
 
+	@Override
 	public boolean isIncludeDefaults() {
 		return Boolean.parseBoolean(getProperty(INCLUDE_DEFAULTS));
 	}
 
+	@Override
 	public void setIncludeDefaults(boolean includeDefaults) {
 		setProperty(INCLUDE_DEFAULTS, Boolean.toString(includeDefaults));
 	}
 
+	@Override
 	public boolean isRemovePermanently() {
 		return Boolean.parseBoolean(getProperty(REMOVE_PERMANENTLY));
 	}
 
+	@Override
 	public void setRemovePermanently(boolean removePermanently) {
 		setProperty(REMOVE_PERMANENTLY, Boolean.toString(removePermanently));
 	}
