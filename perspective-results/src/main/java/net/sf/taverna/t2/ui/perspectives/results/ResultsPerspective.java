bde30be4e23ae7b9ff3ace1ad74de287f42540eb
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-05-13 10:26:07 +0000

    Added results perspective.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-components/trunk@15743 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ perspective-results/src/main/java/net/sf/taverna/t2/ui/perspectives/results/ResultsPerspective.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (C) 2013 The University of Manchester
+ *
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.ui.perspectives.results;
+
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JTextArea;
+
+import net.sf.taverna.t2.workbench.icons.WorkbenchIcons;
+import net.sf.taverna.t2.workbench.ui.zaria.PerspectiveSPI;
+
+/**
+ *
+ *
+ * @author David Withers
+ */
+public class ResultsPerspective implements PerspectiveSPI {
+
+	@Override
+	public String getID() {
+		return this.getClass().getName();
+	}
+
+	@Override
+	public JComponent getPanel() {
+		return new JTextArea("Results go here");
+	}
+
+	@Override
+	public ImageIcon getButtonIcon() {
+		return WorkbenchIcons.resultsPerspectiveIcon;
+	}
+
+	@Override
+	public String getText() {
+		return "Results";
+	}
+
+	@Override
+	public int positionHint() {
+		return 20;
+	}
+
+}
