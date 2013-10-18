9b308dc00718a42dd0a94a70925d37ca29067cca
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-10-18 13:27:12 +0000

    T3-858 Fixed setting constant value for port.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-components/trunk@16192 bf327186-88b3-11dd-a302-d386e5130c1c

--- graph-model/src/main/java/net/sf/taverna/t2/workbench/models/graph/DefaultGraphEventManager.java
+++ graph-model/src/main/java/net/sf/taverna/t2/workbench/models/graph/DefaultGraphEventManager.java
@@ -63,19 +63,24 @@ public class DefaultGraphEventManager implements GraphEventManager {
 	 * Constructs a new instance of GraphEventManager.
 	 *
 	 * @param graphController
-	 * @param component component to use when displaying popup menus
+	 * @param component
+	 *            component to use when displaying popup menus
 	 */
-	public DefaultGraphEventManager(GraphController graphController, Component component, MenuManager menuManager) {
+	public DefaultGraphEventManager(GraphController graphController, Component component,
+			MenuManager menuManager) {
 		this.graphController = graphController;
 		this.component = component;
 		this.menuManager = menuManager;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.sf.taverna.t2.workbench.models.graph.EventGraphManager#mouseClicked(net.sf.taverna.t2.workbench.models.graph.GraphElement, short, boolean, boolean, boolean, int, int, int, int)
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * net.sf.taverna.t2.workbench.models.graph.EventGraphManager#mouseClicked(net.sf.taverna.t2
+	 * .workbench.models.graph.GraphElement, short, boolean, boolean, boolean, int, int, int, int)
 	 */
-	public void mouseClicked(final GraphElement graphElement, short button,
-			boolean altKey, boolean ctrlKey, boolean metaKey, final int x, final int y, int screenX, int screenY) {
+	public void mouseClicked(final GraphElement graphElement, short button, boolean altKey,
+			boolean ctrlKey, boolean metaKey, final int x, final int y, int screenX, int screenY) {
 		Object dataflowObject = graphElement.getWorkflowBean();
 
 		// For both left and right click - add to selection model
@@ -86,114 +91,118 @@ public class DefaultGraphEventManager implements GraphEventManager {
 		// If this was a right click - show a pop-up as well
 		if ((button == 2) || ctrlKey) {
 			if (dataflowObject == null) {
-				menu = menuManager.createContextMenu(graphController.getWorkflow(), graphController.getWorkflow(), component);
+				menu = menuManager.createContextMenu(graphController.getWorkflow(),
+						graphController.getWorkflow(), component);
 			} else {
-				menu = menuManager.createContextMenu(graphController.getWorkflow(), dataflowObject, component);
-			}
-			if (dataflowObject instanceof Processor) {
-				final Processor processor = (Processor) dataflowObject;
-				ProcessorBinding processorBinding = scufl2Tools.processorBindingForProcessor(processor, graphController.getProfile());
-				final Activity activity = processorBinding.getBoundActivity();
-				menu = menuManager.createContextMenu(graphController.getWorkflow(), processor, component);
-				if (menu == null) {
-					menu = new JPopupMenu();
-				}
-				if (graphElement instanceof GraphNode) {
-					boolean expanded = false;
-					if (graphController.getPortStyle(processor).equals(PortStyle.NONE)) {
-						menu.addSeparator();
-						menu.add(new JMenuItem(new AbstractAction("Show ports") {
-							public void actionPerformed(ActionEvent arg0) {
-								graphController.setPortStyle(processor, PortStyle.ALL);
-								graphController.redraw();
-							}
-						}));
-					} else if (graphController.getPortStyle(processor).equals(PortStyle.ALL)) {
-						menu.addSeparator();
-						menu.add(new JMenuItem(new AbstractAction("Hide ports") {
-							public void actionPerformed(ActionEvent arg0) {
-								graphController.setPortStyle(processor, PortStyle.NONE);
-								graphController.redraw();
-							}
-						}));
+				menu = menuManager.createContextMenu(graphController.getWorkflow(), dataflowObject,
+						component);
+				if (dataflowObject instanceof Processor) {
+					final Processor processor = (Processor) dataflowObject;
+					ProcessorBinding processorBinding = scufl2Tools.processorBindingForProcessor(
+							processor, graphController.getProfile());
+					final Activity activity = processorBinding.getBoundActivity();
+					if (menu == null) {
+						menu = new JPopupMenu();
 					}
-					if (activity.getType().equals(NESTED_WORKFLOW_URI)) {
+					if (graphElement instanceof GraphNode) {
+						boolean expanded = false;
+						if (graphController.getPortStyle(processor).equals(PortStyle.NONE)) {
+							menu.addSeparator();
+							menu.add(new JMenuItem(new AbstractAction("Show ports") {
+								public void actionPerformed(ActionEvent arg0) {
+									graphController.setPortStyle(processor, PortStyle.ALL);
+									graphController.redraw();
+								}
+							}));
+						} else if (graphController.getPortStyle(processor).equals(PortStyle.ALL)) {
+							menu.addSeparator();
+							menu.add(new JMenuItem(new AbstractAction("Hide ports") {
+								public void actionPerformed(ActionEvent arg0) {
+									graphController.setPortStyle(processor, PortStyle.NONE);
+									graphController.redraw();
+								}
+							}));
+						}
+						if (activity.getType().equals(NESTED_WORKFLOW_URI)) {
+							menu.addSeparator();
+							menu.add(new JMenuItem(new AbstractAction("Show nested workflow") {
+								public void actionPerformed(ActionEvent arg0) {
+									graphController.setExpandNestedDataflow(activity, true);
+									graphController.redraw();
+								}
+							}));
+						}
 						menu.addSeparator();
-						menu.add(new JMenuItem(new AbstractAction("Show nested workflow") {
-							public void actionPerformed(ActionEvent arg0) {
-								graphController.setExpandNestedDataflow(activity, true);
-								graphController.redraw();
-							}
-						}));
-					}
-					menu.addSeparator();
-					GraphNode graphNode = (GraphNode) graphElement;
-					List<GraphNode> sourceNodes = graphNode.getSourceNodes();
-					if (sourceNodes.size() > 0) {
-						if (sourceNodes.size() == 1) {
-							final GraphNode sourceNode = sourceNodes.get(0);
-							if (sourceNode.getLabel() != null) {
-								menu.add(new JMenuItem(new AbstractAction(
-										"Link from output '"
-												+ sourceNode.getLabel() + "'") {
-									public void actionPerformed(ActionEvent arg0) {
-										graphController.startEdgeCreation(
-												sourceNode, new Point(x, y));
-									}
+						GraphNode graphNode = (GraphNode) graphElement;
+						List<GraphNode> sourceNodes = graphNode.getSourceNodes();
+						if (sourceNodes.size() > 0) {
+							if (sourceNodes.size() == 1) {
+								final GraphNode sourceNode = sourceNodes.get(0);
+								if (sourceNode.getLabel() != null) {
+									menu.add(new JMenuItem(new AbstractAction("Link from output '"
+											+ sourceNode.getLabel() + "'") {
+										public void actionPerformed(ActionEvent arg0) {
+											graphController.startEdgeCreation(sourceNode,
+													new Point(x, y));
+										}
 
-								}));
-							}
-						} else {
-							JMenu linkMenu = new JMenu("Link from output...");
-							menu.add(linkMenu);
-							for (final GraphNode sourceNode : sourceNodes) {
-								linkMenu.add(new JMenuItem(new AbstractAction(sourceNode.getLabel()) {
-									public void actionPerformed(ActionEvent arg0) {
-										graphController.startEdgeCreation(sourceNode, new Point(x, y));
-									}
+									}));
+								}
+							} else {
+								JMenu linkMenu = new JMenu("Link from output...");
+								menu.add(linkMenu);
+								for (final GraphNode sourceNode : sourceNodes) {
+									linkMenu.add(new JMenuItem(new AbstractAction(sourceNode
+											.getLabel()) {
+										public void actionPerformed(ActionEvent arg0) {
+											graphController.startEdgeCreation(sourceNode,
+													new Point(x, y));
+										}
 
-								}));
+									}));
+								}
 							}
 						}
-					}
-					List<GraphNode> sinkNodes = graphNode.getSinkNodes();
-					if (sinkNodes.size() > 0) {
-						if (sinkNodes.size() == 1) {
-							final GraphNode sinkNode = sinkNodes.get(0);
-							if (sinkNode.getLabel() != null) {
-								menu.add(new JMenuItem(new AbstractAction(
-										"Link to input '" + sinkNode.getLabel()
-												+ "'") {
-									public void actionPerformed(ActionEvent arg0) {
-										graphController.startEdgeCreation(
-												sinkNode, new Point(x, y));
-									}
+						List<GraphNode> sinkNodes = graphNode.getSinkNodes();
+						if (sinkNodes.size() > 0) {
+							if (sinkNodes.size() == 1) {
+								final GraphNode sinkNode = sinkNodes.get(0);
+								if (sinkNode.getLabel() != null) {
+									menu.add(new JMenuItem(new AbstractAction("Link to input '"
+											+ sinkNode.getLabel() + "'") {
+										public void actionPerformed(ActionEvent arg0) {
+											graphController.startEdgeCreation(sinkNode, new Point(
+													x, y));
+										}
 
-								}));
-							}
-						} else {
-							JMenu linkMenu = new JMenu("Link to input...");
-							menu.add(linkMenu);
-							for (final GraphNode sinkNode : sinkNodes) {
-								linkMenu.add(new JMenuItem(new AbstractAction(sinkNode.getLabel()) {
+									}));
+								}
+							} else {
+								JMenu linkMenu = new JMenu("Link to input...");
+								menu.add(linkMenu);
+								for (final GraphNode sinkNode : sinkNodes) {
+									linkMenu.add(new JMenuItem(new AbstractAction(sinkNode
+											.getLabel()) {
 
-									public void actionPerformed(ActionEvent arg0) {
-										graphController.startEdgeCreation(sinkNode, new Point(x, y));
-									}
+										public void actionPerformed(ActionEvent arg0) {
+											graphController.startEdgeCreation(sinkNode, new Point(
+													x, y));
+										}
 
-								}));
+									}));
+								}
 							}
 						}
-					}
-				} else if (graphElement instanceof Graph) {
-					if (activity.getType().equals(NESTED_WORKFLOW_URI)) {
-						menu.addSeparator();
-						menu.add(new JMenuItem(new AbstractAction("Hide nested workflow") {
-							public void actionPerformed(ActionEvent arg0) {
-								graphController.setExpandNestedDataflow(activity, false);
-								graphController.redraw();
-							}
-						}));
+					} else if (graphElement instanceof Graph) {
+						if (activity.getType().equals(NESTED_WORKFLOW_URI)) {
+							menu.addSeparator();
+							menu.add(new JMenuItem(new AbstractAction("Hide nested workflow") {
+								public void actionPerformed(ActionEvent arg0) {
+									graphController.setExpandNestedDataflow(activity, false);
+									graphController.redraw();
+								}
+							}));
+						}
 					}
 				}
 			}
@@ -201,56 +210,60 @@ public class DefaultGraphEventManager implements GraphEventManager {
 				final Point p = new Point(screenX, screenY);
 				SwingUtilities.convertPointFromScreen(p, component);
 				SwingUtilities.invokeLater(new Runnable() {
-		            public void run(){
-		            	menu.show(component, p.x, p.y);
-		            }
-		        });
+					public void run() {
+						menu.show(component, p.x, p.y);
+					}
+				});
 			}
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see net.sf.taverna.t2.workbench.models.graph.EventGraphManager#mouseDown(net.sf.taverna.t2.workbench.models.graph.GraphElement, short, boolean, boolean, boolean, int, int, int, int)
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * net.sf.taverna.t2.workbench.models.graph.EventGraphManager#mouseDown(net.sf.taverna.t2.workbench
+	 * .models.graph.GraphElement, short, boolean, boolean, boolean, int, int, int, int)
 	 */
-	public void mouseDown(GraphElement graphElement, short button,
-			boolean altKey, boolean ctrlKey, boolean metaKey, int x,
-			int y, int screenX, int screenY) {
+	public void mouseDown(GraphElement graphElement, short button, boolean altKey, boolean ctrlKey,
+			boolean metaKey, int x, int y, int screenX, int screenY) {
 		if (button == 0) {
 			graphController.startEdgeCreation(graphElement, new Point(x, y));
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see net.sf.taverna.t2.workbench.models.graph.EventGraphManager#mouseUp(net.sf.taverna.t2.workbench.models.graph.GraphElement, short, boolean, boolean, boolean, int, int, int, int)
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * net.sf.taverna.t2.workbench.models.graph.EventGraphManager#mouseUp(net.sf.taverna.t2.workbench
+	 * .models.graph.GraphElement, short, boolean, boolean, boolean, int, int, int, int)
 	 */
-	public void mouseUp(GraphElement graphElement, short button,
-			boolean altKey, boolean ctrlKey, boolean metaKey, final int x,
-			final int y, int screenX, int screenY) {
+	public void mouseUp(GraphElement graphElement, short button, boolean altKey, boolean ctrlKey,
+			boolean metaKey, final int x, final int y, int screenX, int screenY) {
 		if (button == 0) {
 			graphController.stopEdgeCreation(graphElement, new Point(screenX, screenY));
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see net.sf.taverna.t2.workbench.models.graph.EventGraphManager#mouseMoved(net.sf.taverna.t2.workbench.models.graph.GraphElement, short, boolean, boolean, boolean, int, int, int, int)
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * net.sf.taverna.t2.workbench.models.graph.EventGraphManager#mouseMoved(net.sf.taverna.t2.workbench
+	 * .models.graph.GraphElement, short, boolean, boolean, boolean, int, int, int, int)
 	 */
-	public void mouseMoved(GraphElement graphElement, short button,
-			boolean altKey, boolean ctrlKey, boolean metaKey, int x,
-			int y, int screenX, int screenY) {
+	public void mouseMoved(GraphElement graphElement, short button, boolean altKey,
+			boolean ctrlKey, boolean metaKey, int x, int y, int screenX, int screenY) {
 		graphController.moveEdgeCreationTarget(graphElement, new Point(x, y));
 	}
 
-	public void mouseOver(GraphElement graphElement, short button,
-			boolean altKey, boolean ctrlKey, boolean metaKey, int x, int y,
-			int screenX, int screenY) {
+	public void mouseOver(GraphElement graphElement, short button, boolean altKey, boolean ctrlKey,
+			boolean metaKey, int x, int y, int screenX, int screenY) {
 		if (graphElement.getWorkflowBean() != null) {
 			graphElement.setActive(true);
 		}
 	}
 
-	public void mouseOut(GraphElement graphElement, short button,
-			boolean altKey, boolean ctrlKey, boolean metaKey, int x, int y,
-			int screenX, int screenY) {
+	public void mouseOut(GraphElement graphElement, short button, boolean altKey, boolean ctrlKey,
+			boolean metaKey, int x, int y, int screenX, int screenY) {
 		if (graphElement.getWorkflowBean() != null) {
 			graphElement.setActive(false);
 		}
