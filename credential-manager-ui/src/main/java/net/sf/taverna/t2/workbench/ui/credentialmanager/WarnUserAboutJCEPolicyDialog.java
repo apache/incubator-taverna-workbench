22c6b7daf322df2ebb1ec558726db6b4fe523c6f
alex@mygrid.org.uk <alex@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2014-01-16 15:55:14 +0000

    Changed how we check if strong crypto policy is installed - using the 'good' secure cipher AES as er http://stackoverflow.com/questions/7953567/checking-if-unlimited-cryptography-is-available.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-components/branches/maintenance@16523 bf327186-88b3-11dd-a302-d386e5130c1c

--- credential-manager-ui/src/main/java/net/sf/taverna/t2/workbench/ui/credentialmanager/WarnUserAboutJCEPolicyDialog.java
+++ credential-manager-ui/src/main/java/net/sf/taverna/t2/workbench/ui/credentialmanager/WarnUserAboutJCEPolicyDialog.java
@@ -32,7 +32,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.Cipher;
@@ -56,39 +55,42 @@ import net.sf.taverna.t2.workbench.helper.NonBlockedHelpEnabledDialog;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
-
 /**
- * Dialog that warns user that they need to install unlimited cryptography strength policy for Java.
- *  
+ * Dialog that warns user that they need to install unlimited cryptography
+ * strength policy for Java.
+ * 
  * @author Alex Nenadic
- *
+ * 
  */
 @SuppressWarnings("serial")
 public class WarnUserAboutJCEPolicyDialog extends NonBlockedHelpEnabledDialog {
-	
-	private static Logger logger = Logger.getLogger(WarnUserAboutJCEPolicyDialog.class);
-	
+
+	private static Logger logger = Logger
+			.getLogger(WarnUserAboutJCEPolicyDialog.class);
+
 	private JCheckBox doNotWarnMeAgainCheckBox;
-	
-	public WarnUserAboutJCEPolicyDialog(){
-		super((Frame)null, "Java Unlimited Strength Cryptography Policy Warning", true);
+
+	public WarnUserAboutJCEPolicyDialog() {
+		super((Frame) null,
+				"Java Unlimited Strength Cryptography Policy Warning", true);
 		initComponents();
 	}
 
 	// For testing
-	public static void main (String[] args){
+	public static void main(String[] args) {
 		WarnUserAboutJCEPolicyDialog dialog = new WarnUserAboutJCEPolicyDialog();
 		dialog.setVisible(true);
 	}
-	
+
 	private void initComponents() {
 		// Base font for all components on the form
 		Font baseFont = new JLabel("base font").getFont().deriveFont(11f);
-		
+
 		// Message saying that updates are available
 		JPanel messagePanel = new JPanel(new BorderLayout());
-		messagePanel.setBorder(new CompoundBorder(new EmptyBorder(10,10,10,10), new EtchedBorder(EtchedBorder.LOWERED)));
-		
+		messagePanel.setBorder(new CompoundBorder(new EmptyBorder(10, 10, 10,
+				10), new EtchedBorder(EtchedBorder.LOWERED)));
+
 		JEditorPane message = new JEditorPane();
 		message.setEditable(false);
 		message.setBackground(this.getBackground());
@@ -96,33 +98,36 @@ public class WarnUserAboutJCEPolicyDialog extends NonBlockedHelpEnabledDialog {
 		HTMLEditorKit kit = new HTMLEditorKit();
 		message.setEditorKit(kit);
 		StyleSheet styleSheet = kit.getStyleSheet();
-		//styleSheet.addRule("body {font-family:"+baseFont.getFamily()+"; font-size:"+baseFont.getSize()+";}"); // base font looks bigger when rendered as HTML
-		styleSheet.addRule("body {font-family:"+baseFont.getFamily()+"; font-size:10px;}");
+		// styleSheet.addRule("body {font-family:"+baseFont.getFamily()+"; font-size:"+baseFont.getSize()+";}");
+		// // base font looks bigger when rendered as HTML
+		styleSheet.addRule("body {font-family:" + baseFont.getFamily()
+				+ "; font-size:10px;}");
 		Document doc = kit.createDefaultDocument();
 		message.setDocument(doc);
-		message.setText("<html><body>In order for Taverna's security features to function properly - you need to install<br>" +
-				"'Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy'. <br><br>" +
-				"If you do not already have it, for <b>Java 6</b> you can get it from:<br>" +
-				"<a href=\"http://www.oracle.com/technetwork/java/javase/downloads/index.html\">http://www.oracle.com/technetwork/java/javase/downloads/index.html</a><br<br>" +	
-				"Installation instructions are contained in the bundle you download."+
-			"</body><html>");
+		message.setText("<html><body>In order for Taverna's security features to function properly - you need to install<br>"
+				+ "'Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy'. <br><br>"
+				+ "If you do not already have it, for <b>Java 7</b> you can get it from:<br>"
+				+ "<a href=\"http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html\">http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html</a><br<br>"
+				+ "Installation instructions are contained in the bundle you download."
+				+ "</body><html>");
 		message.addHyperlinkListener(new HyperlinkListener() {
 			public void hyperlinkUpdate(HyperlinkEvent he) {
 				HyperlinkEvent.EventType type = he.getEventType();
-			    if (type == HyperlinkEvent.EventType.ACTIVATED) {
+				if (type == HyperlinkEvent.EventType.ACTIVATED) {
 					// Open a Web browser
-					try{
+					try {
 						Desktop.getDesktop().browse(he.getURL().toURI());
 
-					}catch(Exception ex){
-						logger.error("User registration: Failed to launch browser to show terms and conditions at " + he.getURL().toString());
+					} catch (Exception ex) {
+						logger.error("User registration: Failed to launch browser to show terms and conditions at "
+								+ he.getURL().toString());
 					}
-			    }				
+				}
 			}
-		});		
-		message.setBorder(new EmptyBorder(5,5,5,5));
+		});
+		message.setBorder(new EmptyBorder(5, 5, 5, 5));
 		messagePanel.add(message, BorderLayout.CENTER);
-		
+
 		doNotWarnMeAgainCheckBox = new JCheckBox("Do not warn me again");
 		doNotWarnMeAgainCheckBox.setFont(baseFont.deriveFont(12f));
 		messagePanel.add(doNotWarnMeAgainCheckBox, BorderLayout.SOUTH);
@@ -131,14 +136,14 @@ public class WarnUserAboutJCEPolicyDialog extends NonBlockedHelpEnabledDialog {
 		JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		JButton okButton = new JButton("OK");
 		okButton.setFont(baseFont);
-		okButton.addActionListener(new ActionListener(){
+		okButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				okPressed();
 			}
 		});
-		
+
 		buttonsPanel.add(okButton);
-		
+
 		getContentPane().setLayout(new BorderLayout());
 		getContentPane().add(messagePanel, BorderLayout.CENTER);
 		getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
@@ -153,59 +158,61 @@ public class WarnUserAboutJCEPolicyDialog extends NonBlockedHelpEnabledDialog {
 		setSize(getPreferredSize());
 	}
 
-
 	private static final String DO_NOT_WARN_ABOUT_JCE_POLICY = "do_not_warn_about_JCE_policy";
-	public static File doNotWarnUserAboutJCEPolicyFile = new File(CMUtils.getCredentialManagerDefaultDirectory(),DO_NOT_WARN_ABOUT_JCE_POLICY);
-	public static boolean warnedUser = false; // have we already warned user for this run
+	public static File doNotWarnUserAboutJCEPolicyFile = new File(
+			CMUtils.getCredentialManagerDefaultDirectory(),
+			DO_NOT_WARN_ABOUT_JCE_POLICY);
+	public static boolean warnedUser = false; // have we already warned user for
+												// this run
+
 	/**
-	 * Warn user that they need to install Java Cryptography 
-	 * Extension (JCE) Unlimited Strength Jurisdiction Policy 
-	 * if they want Credential Manager to function properly. 
+	 * Warn user that they need to install Java Cryptography Extension (JCE)
+	 * Unlimited Strength Jurisdiction Policy if they want Credential Manager to
+	 * function properly.
 	 */
-	public static void warnUserAboutJCEPolicy(){
-		
+	public static void warnUserAboutJCEPolicy() {
+
 		boolean alreadyInstalled = false;
-		
 
-			try {
-				final int maxAllowedKeyLength = Cipher.getMaxAllowedKeyLength("PBEWithSHAAndTwofish-CBC");
-				alreadyInstalled = maxAllowedKeyLength == Integer.MAX_VALUE;
-			} catch (NoSuchAlgorithmException e) {
-				logger.error("Algorithm PBEWithSHAAndTwofish-CBC could not be found");
-			}
+		try {
+			final int maxAllowedKeyLength = Cipher
+					.getMaxAllowedKeyLength("AES");
+			alreadyInstalled = maxAllowedKeyLength > 128;
+		} catch (NoSuchAlgorithmException e) {
+			logger.error("Algorithm AES could not be found");
+		}
 
-
-		
 		// Do not pop up a dialog if we are running headlessly.
-		// If we have warned the user and they do not want us to remind them again - exit.
-		if (alreadyInstalled || warnedUser || GraphicsEnvironment.isHeadless() || doNotWarnUserAboutJCEPolicyFile.exists()){
+		// If we have warned the user and they do not want us to remind them
+		// again - exit.
+		if (alreadyInstalled || warnedUser || GraphicsEnvironment.isHeadless()
+				|| doNotWarnUserAboutJCEPolicyFile.exists()) {
 			return;
 		}
 
 		WarnUserAboutJCEPolicyDialog warnDialog = new WarnUserAboutJCEPolicyDialog();
 		warnDialog.setVisible(true);
 		warnedUser = true;
-		
+
 	}
-	
-	
-	protected void okPressed(){
-		if (doNotWarnMeAgainCheckBox.isSelected()){
+
+	protected void okPressed() {
+		if (doNotWarnMeAgainCheckBox.isSelected()) {
 			try {
 				FileUtils.touch(doNotWarnUserAboutJCEPolicyFile);
 			} catch (IOException ioex) {
-				logger.error("Failed to touch the 'Do not want me about JCE unilimited security policy file.",
-								ioex);
+				logger.error(
+						"Failed to touch the 'Do not want me about JCE unilimited security policy file.",
+						ioex);
 			}
 		}
 		closeDialog();
 
 	}
-	
+
 	private void closeDialog() {
 		setVisible(false);
 		dispose();
 	}
 
 }
-
