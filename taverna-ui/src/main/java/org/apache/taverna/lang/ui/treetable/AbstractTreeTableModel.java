1f98c7366002e464bfdef1aa55d5c3d0d8ed03df
Stian Soiland-Reyes <stain@apache.org>
2016-09-07 15:03:01 +0100

    License header indenting

--- taverna-ui/src/main/java/org/apache/taverna/lang/ui/treetable/AbstractTreeTableModel.java
+++ taverna-ui/src/main/java/org/apache/taverna/lang/ui/treetable/AbstractTreeTableModel.java
@@ -1,16 +1,21 @@
 /*
- * @(#)AbstractTreeTableModel.java	1.2 98/10/27
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Copyright 1997, 1998 by Sun Microsystems, Inc.,
- * 901 San Antonio Road, Palo Alto, California, 94303, U.S.A.
- * All rights reserved.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * This software is the confidential and proprietary information
- * of Sun Microsystems, Inc. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with Sun.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+/*
+
 package org.apache.taverna.lang.ui.treetable;
 
 import javax.swing.event.EventListenerList;
